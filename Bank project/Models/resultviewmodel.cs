a01bd24635f57fc5a8567f99d694378f69d71e0b
suman kore <sumankore32@gmail.com>
2022-06-19 15:37:20 +0530

    tried paging  but only page is shown. Unable to update records  after placing 2 models in one model

--- Bank project/Models/resultviewmodel.cs
+++ Bank project/Models/resultviewmodel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using PagedList;
+using PagedList.Mvc;
 
 namespace Bank_project.Models
 {
@@ -12,6 +14,11 @@ namespace Bank_project.Models
         public string id { get; set; }
         public IEnumerable<Registration> Registerview { get; set; }
         public IEnumerable<Transactions> Transactionsview { get; set; }
+        public List<Registration> Registerviewlist { get; set; }
+        public List<Transactions> Transactionsviewlist { get; set; }
+        public IPagedList<Registration> Registerviewpl { get; set; }
+        public IPagedList<Transactions> Transactionsviewpl { get; set; }
+
         //public List<Registration> Registerview { get; set; }
         //public List<Transactions> Transactionsview { get; set; }
 
